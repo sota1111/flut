07d5d7346620ed418a5eeb5d036d6a9333eb4ed4
sota1111 <sota.moro@gmail.com>
2023-04-16 10:23:25 +0900

    socket client実装

---
+++ python_server/socket_server.py
@@ -0,0 +1,13 @@
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        print(f"Received message: {message}")
+        message = "res:" + message
+        await websocket.send(f"Echo: {message}")
+
+start_server = websockets.serve(echo, "localhost", 8080)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ socket1/socket1/android/app/src/main/kotlin/com/example/socket1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.socket1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
