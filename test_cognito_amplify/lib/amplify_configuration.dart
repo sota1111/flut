9c178a0fe97243d1931800e608add78ab6c173d7
sota1111 <sota.moro@gmail.com>
2023-08-01 16:54:48 +0900

    initial commit

---
+++ test_cognito_amplify/lib/amplify_configuration.dart
@@ -0,0 +1,36 @@
+const amplify_config = {
+  "auth": {
+    "plugins": {
+      "awsCognitoAuthPlugin": {
+        "CognitoUserPool": {
+          "Default": {
+            "PoolId": "hogehoge",
+            "AppClientId": "hogehoge",
+            "Region": "ap-northeast-1"
+          }
+        },
+        "Auth": {
+          "Default": {
+            "authenticationFlowType": "USER_SRP_AUTH",
+            "socialProviders": [],
+            "usernameAttributes": [],
+            "signupAttributes": [
+              "email"
+            ],
+            "passwordProtectionSettings": {
+              "passwordPolicyMinLength": 8,
+              "passwordPolicyCharacters": []
+            },
+            "mfaConfiguration": "OFF",
+            "mfaTypes": [
+              "SMS"
+            ],
+            "verificationMechanisms": [
+              "EMAIL"
+            ]
+          }
+        }
+      }
+    }
+  }
+};
