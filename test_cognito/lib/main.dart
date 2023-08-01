8735c15c9486a957d3aebea735d1889d663ba989
sota1111 <sota.moro@gmail.com>
2023-08-01 15:54:39 +0900

    認証成功画面、再設定画面追加

--- test_cognito/lib/main.dart
+++ test_cognito/lib/main.dart
@@ -92,9 +92,100 @@ class MyApp extends StatelessWidget {
                       username: username, password: password);
                   await cognitoUser.authenticateUser(authDetails);
                   print('Sign in successful');
-                } on CognitoUserException catch (e) {
-                  // エラーメッセージを表示します
-                  errorController.value = e.message!;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SuccessScreen()),
+                  );
+                } on CognitoUserNewPasswordRequiredException catch (e) {
+                  // New password required, redirect to set new password screen.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => NewPasswordScreen(username, userPool)),
+                  );
+                } on CognitoClientException catch (e) {
+                  if (e.code == 'NotAuthorizedException') {
+                    errorController.value = 'NotAuthorizedException';
+                  } else {
+                    errorController.value = e.message!;
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SuccessScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Success'),
+      ),
+      body: Center(
+        child: Text('Login Successful'),
+      ),
+    );
+  }
+}
+
+class NewPasswordScreen extends StatefulWidget {
+  final String username;
+  final CognitoUserPool userPool;
+
+  NewPasswordScreen(this.username, this.userPool);
+
+  @override
+  _NewPasswordScreenState createState() => _NewPasswordScreenState();
+}
+
+class _NewPasswordScreenState extends State<NewPasswordScreen> {
+  final newPasswordController = TextEditingController();
+  final errorController = ValueNotifier<String>('');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Set New Password"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: newPasswordController,
+              decoration: InputDecoration(
+                labelText: "New Password",
+              ),
+              obscureText: true,
+            ),
+            ValueListenableBuilder(
+              valueListenable: errorController,
+              builder: (context, String value, _) {
+                return Text(value);
+              },
+            ),
+            ElevatedButton(
+              child: Text("Set New Password"),
+              onPressed: () async {
+                String newPassword = newPasswordController.text;
+
+                try {
+                  final cognitoUser = new CognitoUser(widget.username, widget.userPool);
+                  final authDetails = new AuthenticationDetails(
+                      username: widget.username, password: newPassword);
+                  await cognitoUser.authenticateUser(authDetails);
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SuccessScreen()),
+                  );
                 } catch (e) {
                   print(e);
                 }
