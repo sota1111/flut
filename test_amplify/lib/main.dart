9020b2dfe6974bc143a30d739a878ab47dc32a10
sota1111 <sota.moro@gmail.com>
2023-08-04 14:56:20 +0900

    実行まで完了

--- test_amplify/lib/main.dart
+++ test_amplify/lib/main.dart
@@ -1,54 +1,56 @@
-import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:flutter/material.dart';
+
 import 'amplifyconfiguration.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
+class _MyAppState extends State<MyApp> {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  initState() {
+  void initState() {
     super.initState();
     _configureAmplify();
   }
 
   Future<void> _configureAmplify() async {
-    final authPlugin = AmplifyAuthCognito();
-    await Amplify.addPlugin(authPlugin);
+    // Add the following line to add API plugin to your app.
+    // Auth plugin needed for IAM authorization mode, which is default for REST API.
+    final auth = AmplifyAuthCognito();
+    final api = AmplifyAPI();
+    await Amplify.addPlugins([api, auth]);
 
     try {
       await Amplify.configure(amplifyconfig);
     } on AmplifyAlreadyConfiguredException {
-      print("Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+      safePrint(
+          'Tried to reconfigure Amplify; this can occur when your app restarts on Android.');
     }
+  }
+
+  Future<void> onTestApi() async {
+    // Edit this function with next steps.
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Amplify App"),
-      ),
-      body: Center(
-        child: Text("Welcome to my Amplify App!"),
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ElevatedButton(
+            onPressed: onTestApi,
+            child: const Text('Rest API'),
+          ),
+        ),
       ),
     );
   }
