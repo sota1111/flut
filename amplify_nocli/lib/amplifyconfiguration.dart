c313cace5a4c13994f50fc6fd27f9b9c7c453f95
sota1111 <sota.moro@gmail.com>
2023-08-06 23:29:58 +0900

    initial commit

---
+++ amplify_nocli/lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = '''{
+  "auth": {
+    "plugins": {
+      "awsCognitoAuthPlugin": {
+        "CognitoUserPool": {
+          "Default": {
+            "PoolId": "ap-northeast-1_hvrlhTL9X",
+            "AppClientId": "db45ulgkpq59deqhq5ikvvqlr",
+            "Region": "ap-northeast-1"
+          }
+        },
+        "Auth": {
+          "Default": {
+            "authenticationFlowType": "USER_SRP_AUTH",
+            "socialProviders": [],
+            "usernameAttributes": [],
+            "signupAttributes": [
+              "email"
+            ],
+            "passwordProtectionSettings": {
+              "passwordPolicyMinLength": 8,
+              "passwordPolicyCharacters": []
+            },
+            "mfaConfiguration": "OFF",
+            "mfaTypes": [
+              "SMS"
+            ],
+            "verificationMechanisms": [
+              "EMAIL"
+            ]
+          }
+        }
+      }
+    }
+  }
+}''';
