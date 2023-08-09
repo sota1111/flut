bb738c6dd7ad5c68e7cc4062e293829fd47fef8f
sota1111 <sota.moro@gmail.com>
2023-08-10 08:18:06 +0900

    色々試したが認証できない

--- test_cognito_sam/lib/main.dart
+++ test_cognito_sam/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 
 import 'amplifyconfiguration.dart';
@@ -22,23 +23,26 @@ class _MyAppState extends State<MyApp> {
     _configureAmplify();
   }
 
-  Future<void> _configureAmplify() async {
+  void _configureAmplify() async {
     try {
-      final auth = AmplifyAuthCognito();
-      await Amplify.addPlugin(auth);
-
-      // call Amplify.configure to use the initialized categories in your app
+      await Amplify.addPlugin(AmplifyAuthCognito());
       await Amplify.configure(amplifyconfig);
+      safePrint('Successfully configured');
     } on Exception catch (e) {
-      safePrint('An error occurred configuring Amplify: $e');
+      safePrint('Error configuring Amplify: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: SizedBox(),
+    return Authenticator(
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        home: const Scaffold(
+          body: Center(
+            child: Text('You are logged in!'),
+          ),
+        ),
       ),
     );
   }
