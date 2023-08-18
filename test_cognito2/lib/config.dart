4f050b229cb72bf88c8426669654a39d9e6a95db
sota1111 <sota.moro@gmail.com>
2023-08-18 15:10:54 +0900

    リファクタリング

--- test_cognito2/lib/config.dart
+++ test_cognito2/lib/config.dart
@@ -1,8 +1,5 @@
 import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 
-const String email = 'sota.moro@gmail.com';
-
-const String cognitoDomain = 'chat';
 const String cognitoUserPoolId = 'ap-northeast-1_hvrlhTL9X';
 const String cognitoClientId = 'db45ulgkpq59deqhq5ikvvqlr';
 const String cognitoFedIdPoolId = 'ap-northeast-1:b39199c7-8bd9-4568-be2d-7eee202d49e3';
--- test_cognito2/lib/main.dart
+++ test_cognito2/lib/main.dart
@@ -1,50 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart' as auth;
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:flutter/material.dart';
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
-import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 import 'package:http/http.dart' as http;
 import 'amplifyconfiguration.dart';
 import 'dart:convert';
 import 'config.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  CognitoUserSession? session;
-
   String _responseText = "";
   String _apiResult = "";
+
   @override
   void initState() {
     super.initState();
     _configureAmplify();
-  }
-
-  Future<void> _getFleetData() async {
-    var url = Uri.parse('https://aelmwouyzf.execute-api.ap-northeast-1.amazonaws.com/default/getFleetDDB');
-    print("debug");
-    var response = await http.post(
-      url,
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'Date': "2023-06-01",
-      }),
-    );
-    debugPrint('Response status code: ${response.statusCode}');
   }
 
   void _configureAmplify() async {
@@ -52,38 +34,17 @@ class _MyAppState extends State<MyApp> {
       final authPlugin = auth.AmplifyAuthCognito();
       Amplify.addPlugin(authPlugin);
       await Amplify.configure(amplifyconfig);
-      safePrint('Successfully configured');
-    } on Exception catch (e) {
-      safePrint('Error configuring Amplify: $e');
-    }
-  }
-
-  Future<String> fetchIdToken() async {
-    try {
-      final result = await Amplify.Auth.fetchAuthSession() as auth.CognitoAuthSession;
-
-      if (result.userPoolTokensResult.value != null) {
-        return result.userPoolTokensResult.value!.idToken.raw;
-      } else {
-        throw Exception('Failed to fetch user pool tokens');
-      }
+      print('Successfully configured');
     } catch (e) {
-      print('Error fetching auth session: $e');
-      throw e;
+      print('Error configuring Amplify: $e');
     }
   }
-
 
   Future<void> _performAuthorizedGet() async {
-    String? jwtToken;
-    AuthSession session = await Amplify.Auth.fetchAuthSession();
-    final result = await Amplify.Auth.fetchAuthSession() as auth.CognitoAuthSession;
-    jwtToken = result.userPoolTokensResult.value.idToken.raw;
-    await credentials.getAwsCredentials(jwtToken);
+    try {
+      final jwtToken = await fetchIdToken();
+      await credentials.getAwsCredentials(jwtToken);
 
-    final localSession = session;
-
-    if (localSession != null) {
       final AwsSigV4Client awsSigV4Client = AwsSigV4Client(
         credentials.accessKeyId!,
         credentials.secretAccessKey!,
@@ -102,7 +63,7 @@ class _MyAppState extends State<MyApp> {
       );
 
       final headers = {
-        if (jwtToken != null) 'Authorization': jwtToken,
+        'Authorization': jwtToken,
         'header-2': 'two',
       };
 
@@ -115,15 +76,25 @@ class _MyAppState extends State<MyApp> {
       } catch (e) {
         print(e);
       }
+    } catch (e) {
+      print(e);
     }
+  }
+
+  Future<String> fetchIdToken() async {
+    final result = await Amplify.Auth.fetchAuthSession() as auth.CognitoAuthSession;
+    if (result.userPoolTokensResult.value != null) {
+      return result.userPoolTokensResult.value!.idToken.raw;
+    }
+    throw Exception('Failed to fetch user pool tokens');
   }
 
   Future<void> _signOut() async {
     try {
       await Amplify.Auth.signOut();
-      safePrint('Successfully signed out');
-    } on Exception catch (e) {
-      safePrint('Error signing out: $e');
+      print('Successfully signed out');
+    } catch (e) {
+      print('Error signing out: $e');
     }
   }
 
@@ -147,7 +118,7 @@ class _MyAppState extends State<MyApp> {
                 SizedBox(height: 20),
                 Text('API Result: $_apiResult'),
                 ElevatedButton(
-                  onPressed: _getFleetData,
+                  onPressed: () => _getFleetData(),
                   child: Text('Get Fleet Data'),
                 ),
                 SizedBox(height: 20),
@@ -161,5 +132,23 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
+  }
+}
+
+Future<void> _getFleetData() async {
+  var url = Uri.parse('https://aelmwouyzf.execute-api.ap-northeast-1.amazonaws.com/default/getFleetDDB');
+  try {
+    var response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'Date': "2023-06-01",
+      }),
+    );
+    debugPrint('Response status code: ${response.statusCode}');
+  } catch (e) {
+    print(e);
   }
 }
